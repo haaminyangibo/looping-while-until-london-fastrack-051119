3040dba581348901cc971a54be2a744135c102e5
Haami Nyangibo <haaminyangibo@gmail.com>
2019-05-12 10:42:00 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,16 +1,12 @@
 def using_while
   levitation_force = 0
   
-  while levitation_force < 10 do
+   while levitation_force < 10 do
   
-  puts "Wingardium Leviosa"
-   levitation_force += 1
+    puts "Wingardium Leviosa"
+ levitation_force += 1
   end
  
-  
-  
-	
-  #your code here
 end
 
 
